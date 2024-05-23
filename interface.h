3f0b3cc082fbff75ff50a742f746bb470497b7de
Goldensquirrel <goldensquirrel11@gmail.com>
2024-05-23 13:21:12 +0800

    Created Class definitions for Board & Game

--- interface.h
+++ interface.h
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <string>
 
+#include "deque.h"
+#include "robot.h"
+
 using namespace std;
 
 /// @brief
@@ -21,5 +24,43 @@ string repeatChar(const char character, const int numberOfTimes)
 
     return temp;
 }
+
+class Board
+{
+private:
+    int width = 0;
+    int height = 0;
+    string boardBuffer = "";
+
+    // Characters used in the board
+    char EMPTY_SPACE = ' ';
+    char CORNER = '+';
+    char HORIZONTAL_BAR = '-';
+    char VERTICAL_BAR = '|';
+
+public:
+    // Accessors
+    string getBoard();
+
+    // Modifiers
+    void setWidth(int width);
+    void setHeight(int height);
+    void refresh();
+};
+
+class Game
+{
+private:
+    int turn = 0;
+    Board board;
+    int turnLimit = 0;
+    string displayBuffer = "";
+    Deque<Robot> robotDeque;
+
+    void readConfigFile(ifstream &configFile);
+
+public:
+    Game(ifstream &configFile);
+};
 
 #endif
