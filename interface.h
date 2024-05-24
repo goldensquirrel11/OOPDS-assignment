bdb5f77dc40e60c940a54eaf624598a4f9e9dbce
Goldensquirrel <goldensquirrel11@gmail.com>
2024-05-24 09:03:55 +0800

    Added WIP config file reading function

--- interface.h
+++ interface.h
@@ -102,4 +102,79 @@ public:
     Game(ifstream &configFile);
 };
 
+inline void Game::readConfigFile(ifstream &configFile)
+{
+    // TODO: Read in values from config file and call appropriate initializers
+    string input;
+
+    // Read in board width & height
+    getline(configFile, input);
+    input = input.substr(9);
+    board.setWidth(stoi(input.substr(0, input.find(' '))));
+    board.setHeight(stoi(input.substr(input.find(' ') + 1)));
+
+    // Initialize RNG
+    RNG::init(board.getWidth(), board.getHeight());
+
+    // Read in turn limit
+    getline(configFile, input);
+    turnLimit = stoi(input.substr(7));
+
+    // Read in number of robots
+    int robotCount = 0;
+    getline(configFile, input);
+    robotCount = stoi(input.substr(8));
+
+    // Read in each robot
+    string robotType;
+    string robotName;
+    int posX;
+    int posY;
+
+    for (int i = robotCount; i > 0; i--)
+    {
+        getline(configFile, robotType, ' ');
+        getline(configFile, robotName, ' ');
+
+        getline(configFile, input, ' ');
+        if (input == "random")
+            posX = RNG::posX();
+        else
+            posX = stoi(input);
+
+        getline(configFile, input, '\n');
+        if (input == "random")
+            posY = RNG::posY();
+        else
+            posY = stoi(input);
+
+        // TODO: Complete this if-else-if block when all robot classes have been defined
+        if (robotType == "RoboCop")
+        {
+        }
+        else if (robotType == "Terminator")
+        {
+        }
+        else if (robotType == "TerminatorRoboCop")
+        {
+        }
+        else if (robotType == "BlueThunder")
+        {
+        }
+        else if (robotType == "Madbot")
+        {
+        }
+        else if (robotType == "RoboTank")
+        {
+        }
+        else if (robotType == "UltimateRobot")
+        {
+        }
+        else
+        {
+            // TODO: Throw error for undefined robot type
+        }
+    }
+}
+
 #endif
